f7f0972a959386b4fd297ff37e1e0b9aac4b85cb
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2022-12-26 01:14:01 +0200

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const bgColorSpan = document.querySelector(".color");
+const bgBody = document.body;
+
+function getChangeColor() {
+	bgBody.style.backgroundColor = getRandomHexColor();
+	bgColorSpan.textContent = getRandomHexColor();
+}
+
+changeColorBtn.addEventListener("click", getChangeColor);
