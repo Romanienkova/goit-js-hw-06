edd16df83a9b24d0d16cf3b828a537dad3a4c5bd
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2022-12-27 00:32:46 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("input");
+const btnCreate = document.querySelector("[data-create]");
+const btnDestroy = document.querySelector("[data-destroy]");
+const divCreateBoxesEl = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  let divsBoxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = 30 + i * 10 + "px";
+    box.style.height = 30 + i * 10 + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    // const boxSize = 30 + i * 10;
+    // box.style = `width:${boxSize}px; height:${boxSize}px; background-color:getRandomHexColor()`;
+    divsBoxes.push(box);
+  }
+  divCreateBoxesEl.append(...divsBoxes);
+};
+
+const destroyBoxes = () => {
+  divCreateBoxesEl.innerHTML = "";
+};
+
+btnCreate.addEventListener("click", () => createBoxes(inputEl.value));
+btnDestroy.addEventListener("click", destroyBoxes);
